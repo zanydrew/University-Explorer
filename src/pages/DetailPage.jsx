ddf62f9025b1955066727a3cc44180135a21e51c
zanydrew <nizahon00@gmail.com>
2026-04-06 02:45:42 +0700

    implement DetailPage

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,8 +1,22 @@
 import ScholarshipDetailPage from './ScholarshipDetailPage.jsx'
+import UniversityDetailPage from "./UniversityDetailPage.jsx";
 
-export default function DetailPage({ data, onBack, isWishlisted, onToggleWishlist }){
+export default function DetailPage({ data, onBack, isWishlisted, onToggleWishlist }) {
+    const isScholarship = !!data?.title
+
+    if (isScholarship) {
+        return (
+            <ScholarshipDetailPage
+                data={data}
+                onBack={onBack}
+                isWishlisted={isWishlisted}
+                onToggleWishlist={onToggleWishlist}
+            />
+        )
+    }
+
     return (
-        <ScholarshipDetailPage
+        <UniversityDetailPage
             data={data}
             onBack={onBack}
             isWishlisted={isWishlisted}
